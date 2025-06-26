25781aca8f4195a32488ec768ed996d2e39914e5
Guilherme Guedes <gguedesprofissional@gmail.com>
2025-06-26 14:52:58 -0300

    update: ajustado media query para telas acima de 1280px

--- src/pages/home/components/shortAbout/ShortAbout.jsx
+++ src/pages/home/components/shortAbout/ShortAbout.jsx
@@ -1,28 +1,43 @@
-import './ShortAbout.css'
-import PhotoSmile from './img/perfil2.jpg'
+import './ShortAbout.css';
+import PhotoSmile from './img/perfil2.jpg';
 
-export default function ShortAbout(){
-    return(
-        <section className="short-about">
-            <div className="short-content">
-                <div className="about-tittle">
-                    <h2>SOBRE MIM</h2>
-                </div>
-                <div className="about-description">
-                    <p className="description">Sou Web Developer, moro no Rio de Janeiro e estou em busca de oportunidades interessantes e estou aprimorando minhas habilidades com foco em me tornar um Dev Full Stack.</p>
-                    <span>Atualmente  estou focado em aprimorar meus conhecimentos em React.js, onde inclusive essa pagina foi feita com o uso dessa linguagem e meu proximo passo é estudar Node.js para me tornar Dev Full Stack.
-                    <br/>
-                    <br/>
-                    Tenho uma trajetória como DJ, tocando em clubes e também viajando com o Gabriel o Pensador, fazendo parte de sua banda e aprendendo a trabalhar em equipe em diferentes contextos. 
-                    <br/>
-                    <br/>
-                    Além do desenvolvimento web, também faço lives jogando Counter-Strike e sigo dedicado à música, sempre buscando criar experiências únicas nos meus sets. Sou curioso, apaixonado por tecnologia e música, e estou sempre em busca de novas formas de transformar ideias em projetos criativos e impactantes.
-                    </span>
-                    <a href="#">SAIBA MAIS</a>
-                    <img src={PhotoSmile} alt="photo-smile" />
-                </div>
-
-            </div>
-        </section>
-    )
+export default function ShortAbout() {
+	return (
+		<section className="short-about">
+			<div className="short-content">
+				<div className="about-tittle">
+					<h2>SOBRE MIM</h2>
+				</div>
+				<div className="about-description">
+					<p className="description">
+						Sou Web Developer, moro no Rio de Janeiro e estou em busca de
+						oportunidades interessantes, estou aprimorando minhas habilidades
+						com foco em me tornar um Dev Full Stack.
+					</p>
+					<span>
+						Atualmente estou focado em aprimorar meus conhecimentos em React.js,
+						onde inclusive essa pagina foi feita com o uso dessa linguagem e meu
+						proximo passo é estudar Node.js para me tornar Dev Full Stack.
+						<br />
+						<br />
+						Tenho uma trajetória como DJ, tocando em clubes e também viajando
+						com o Gabriel o Pensador, fazendo parte de sua banda e aprendendo a
+						trabalhar em equipe em diferentes contextos.
+						<br />
+						<br />
+						Além do desenvolvimento web, também faço lives jogando
+						Counter-Strike e sigo dedicado à música, sempre buscando criar
+						experiências únicas nos meus sets. Sou curioso, apaixonado por
+						tecnologia e música, e estou sempre em busca de novas formas de
+						transformar ideias em projetos criativos e impactantes.
+					</span>
+					<a href="#">SAIBA MAIS</a>
+					<img
+						src={PhotoSmile}
+						alt="photo-smile"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }
